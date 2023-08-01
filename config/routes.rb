d511afc519761b0d8644e6bfb46f1df8f13faccc
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-01 12:04:30 +0000

    [Add] 編集フォームのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  get 'lists/edit'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'  # URLに:idを用いて記述すると、lists/◯◯/editのすべてのURLが対象になります。
 end
