7dad717a32ba6eb102adec5ae2f11e32b0edc48e
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-01 12:55:03 +0000

    [Add] ルーティングに更新機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'  # URLに:idを用いて記述すると、lists/◯◯/editのすべてのURLが対象になります。
+  patch 'lists/:id' => 'lists#update', as: 'update_list'  # 更新の場合はPATCHで指定します。
 end
