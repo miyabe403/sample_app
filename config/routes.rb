709bf3206655343257e2ed14c7201c6e8db11ef6
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-02 03:27:58 +0000

    [Add] 削除機能のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'lists/:id' => 'lists#show', as: 'list'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'  # URLに:idを用いて記述すると、lists/◯◯/editのすべてのURLが対象になります。
   patch 'lists/:id' => 'lists#update', as: 'update_list'  # 更新の場合はPATCHで指定します。
-  
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'  # HTTPメソッドはDELETE、URLはlists/:idです。名前付きパスは、destroy_listと設定します。
 end
